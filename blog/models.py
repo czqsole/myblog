054bb31f40f23c8bb4da8279d8fe44246580f221
czqsole <czqsole@gmail.com>
2016-01-07 22:50:19 +0800

    add Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    #id =
+    pub_time = models.DateField('date published')
+    title = models.CharField(max_length=100)
+    summary = models.TextField()
+    article = models.TextField()
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import django
+from blog.models import Blog
+from django.utils import timezone
 
-# Create your tests here.
+django.setup()
+Blog.objects.all()
+blog1 = Blog(time = timezone.now(), title = "first test blog", summary = 'blog1', article = "test")
+blog1.save()
+
+Blog.objects.all()
